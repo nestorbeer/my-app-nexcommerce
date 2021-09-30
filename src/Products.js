21d60e968d62b7e4a29ccb94ea742ed386a21129
Nestor Beer <nextor.beer@gmail.com>
2021-09-30 09:08:37 -0300

    1

---
+++ src/Products.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Products(){
+   return(
+      <div className="Products">
+          <p>Aca va la lista de productos</p>
+      </div>
+    );
+}
+export default Products;
